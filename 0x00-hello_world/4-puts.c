335c6fc68e8ebed8b47b8c109a75244a0af829c3
badawiabbass <badawi.nor@gmail.com>
2023-09-07 19:14:54 +0100

    Alway go up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point "Programming is like building a multilingual puzzle"
+ * main - display "Programming is like building a multilingual puzzle"
  * Return: 0 and exit the program
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0):
+	return (0);
 }
