816e569b9761dc7c556ba75653dc1423e2d2a29f
badawiabbass <badawi.nor@gmail.com>
2023-09-07 20:16:52 +0100

    All fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- * Description: print a qute using write function
- * 	ssize-t write(int fd, const void *buf, size-t count);
- *
+ * main - deplay "and that piece of art is useful" in consol
+ * followed by a new line to the stander error
  * Return: 1 (error).
  */
 int main(void)
 {
-	char quo[] = (2,""and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1. quo, 59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
