703dd960ff623b3112d32a529c2c16d3593777e5
badawiabbass <badawi.nor@gmail.com>
2023-09-07 18:25:28 +0100

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /**
  * main - display  "Programming is like building a multilingual puzzle"
  * Return: 0 and exit the program
-int main(void)
+ */
+  int main(void)
   {
 	 puts("\"Programming is like building a multilingual puzzle");
 	 return (0);
