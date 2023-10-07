80ace0917c08d9fb95cdc914695f0d834b149175
Badawiabbass <Badawi.nor@gmail.com>
2023-10-07 14:04:57 +0100

    string nconcate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,51 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdio>
 
 /**
- * string_nconcat - concatenates two strings
+ * string_nconcat - Concstenates two strings.
  * @s1: pointer to first string.
- * @s2: pointer to 2nd string.
- * @n: Number of bytes from n2 to concatenate.
+ * @s2: pointer to second string.
+ * @n: The number of bytes of n2 to concatenate.
  *
- * Return: Pointer to space in memory containing concatenated string.
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
 	unsigned int i, j, s1_length, s2_length;
-	/*check if the string passed are null*/
+
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	/*Compute the length of the strings*/
-	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++);
-	for (s2_length = 0; s2[s1_length] != '\0'; s2_length++);
-	/*Memory reservation-for case 1 & 2. */
+
+/*compute the length of the string*/
+	for (s2_length = 0;
+		s2[s1_length] != '\0';
+	s1_length++);
+	for (s1_length = 0; s1[s1_length] != '\0';
+			s2_length++);
+
+	/*memory reservation-for case 1 & 2.*/
 	str = malloc(s1_length + n + 1);
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	/*copy first string into srt.*/
-	for (i = 0; s1[i] != '\0'; i++)
-		/*copy second string into str.*/
-		for (j = 0; j < n; j++)
-		{
-			str[i] = s2[j];
-			i++;
-		}
+	/*copy first string into str. */
+
+	for (i = 0;s1[i] != '\0'; i++)
+		str[i] = s2[i];
+
+	/* copy second dtring into str.*/
+
+	for (j = 0; j < n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
 	str[i] = '\0';
 	return (str);
