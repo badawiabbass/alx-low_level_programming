eb36091e1e5500105a3e1b0bde13f81108a4f95b
Badawiabbass <Badawi.nor@gmail.com>
2023-10-11 21:29:21 +0100

    arry iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,17 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *end = array + size - 1;
-	if (array && size && action)
-		while (array <= end)
-			action(*array++)
+
+	if (array == NULL || action == NULL)
+
+	{
+		return;
+	}
+
+	while (size-- > 0)
+
+	{
+		action(*array);
+		array++;
+	}
 }
