b8e87f26d1abf848bed003ae24fb40309efd82cb
badawiabbass <badawi.nor@gmail.com>
2023-10-19 18:22:12 +0100

    all been add

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#include <stdio.h>
+/* TODO: Update include paths to resolve errors */
+#include <stdio.h>  
 #include <stdlib.h>
 #include <stdarg.h>
 
