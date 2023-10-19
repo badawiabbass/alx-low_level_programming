f5709ef4c5b423c770de2adfbf1a784e32177db0
badawiabbass <badawi.nor@gmail.com>
2023-10-19 19:08:05 +0100

    Update lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
+/**
+ * @brief List structure
+ * structure for list
+ * @param str - string
+ * @param len - length of string
+ * @param next - pointer to next node
+ * @return pointer to list
+ * 
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+
+} 
+list_t;
+
+
+size_t print_list(const list_t *h);
+size_t print_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t **head);
